deb003a17f294d8e663c069987ddbfe2997b5f68
Matija Rudnik <matija_ru@yahoo.com>
2018-09-17 15:55:04 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LogIn Page</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+</head>
+<body>
+    <div id="main-container">
+        <div class="login">
+            <img src="img/img2.png"  class="avatar" alt="avatar">
+            <h1>Login Here</h1>
+            <form>
+                <p>Username</p>
+                <input type="text" name="uid" placeholder="Enter Username / email">
+                <p>Password</p>
+                <input type="password" name="pwd" placeholder="Enter Password">
+                <button class="btn_submit" type="submit" name="submit"><a href="login.php">Login</a></button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
+
+
+
---
+++ login.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>H&M</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+
+    <h1 class="title">H&M</h1>
+
+    <main class="main">
+
+        <figure class="figure1">
+            <img src="img/sneaky-steve-shoe.png" class="figure-img">
+            <div class="desc">
+                Sneaky Steve Shoe
+            </div>
+        </figure>
+
+        <figure class="figure2">
+            <img src="img/Axel-Arigato-shoe.png" class="figure-img">
+            <div class="desc">
+            Axel Arigato shoe
+            </div>
+        </figure>
+
+        <figure class="figure3">
+            <img src="img/HM-bottle.png" class="figure-img">
+            <div class="desc">
+            HM-bottle
+            </div>
+        </figure>
+
+        <figure class="figure4">
+            <img src="img/hm-box.png" class="figure-img">
+            <div class="desc">
+            Hm box
+            </div>
+        </figure>
+
+        <figure class="figure5">
+        <img src="img/HM-candlebar.png" class="figure-img">
+        <div class="desc">
+            HM candlebar
+            </div>
+        </figure>
+
+        <figure class="figure6">
+            <img src="img/HM-shoe.png" class="figure-img">
+            <div class="desc">
+            HM shoe
+            </div>
+        </figure>
+
+        <figure class="figure7">
+            <img src="img/Onitsuka-shoe.png" class="figure-img">
+            <div class="desc">
+            Onitsuka shoe
+            </div>
+        </figure>
+
+        <figure class="figure8">
+            <img src="img/woden-shoe.png" class="figure-img">
+            <div class="desc">
+            Woden shoe
+            </div>
+        </figure>
+
+
+    </main>
+    </div>
+    
+</body>
+</html>
